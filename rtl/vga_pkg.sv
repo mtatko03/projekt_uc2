ecbc3e5416a337023a476ba617044eb3bfd7067c
awroblewska02 <awroblewska@student.agh.edu.pl>
2024-06-13 14:13:50 +0200

    zmiana rozdzielczosci

--- rtl/vga_pkg.sv
+++ rtl/vga_pkg.sv
@@ -9,10 +9,20 @@
 
 package vga_pkg;
 
-// Parameters for VGA Display 800 x 600 @ 60fps using a 40 MHz clock;
-localparam HOR_PIXELS = 800;
-localparam VER_PIXELS = 600;
+// Parameters for VGA Display 1024 x 768 @ 60fps using a 40 MHz clock;
+localparam HOR_PIXELS = 1024;
+localparam VER_PIXELS = 768;
+localparam HOR_TOTAL_TIME = 1344;
+localparam HOR_BLANK_START = 1024;
+localparam HOR_SYNC_START = 1048;
+localparam VER_TOTAL_TIME = 806;
+localparam VER_BLANK_START = 768;
+localparam VER_SYNC_START = 771;
 
 // Add VGA timing parameters here and refer to them in other modules.
+localparam HOR_SYNC_END = 1184;
+localparam HOR_BLANK_END = 1344;
+localparam VER_SYNC_END = 777;
+localparam VER_BLANK_END = 806;
 
 endpackage
